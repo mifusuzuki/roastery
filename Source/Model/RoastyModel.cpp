cb789cdbafe18c0b7e36f28c9e9ed364e70b2f21
Mifu Suzuki <ms1518@texel43.doc.ic.ac.uk>
2020-11-27 13:48:46 +0000

    first commit - pass all tests

---
+++ Source/Model/RoastyModel.cpp
@@ -0,0 +1,286 @@
+/* Roasty Model Implementation */
+#include "RoastyModel.hpp"
+
+#include <iostream>
+#include <string>
+
+
+void* operator new(size_t size)
+{
+    std::cout << "Allocating " << size << " bytes\n";
+    return malloc(size);
+}
+
+
+/* ============== Bean ================= */
+
+/* user-defined constructor */
+Bean::Bean(std::string inputBeanName) 
+{
+    beanName = inputBeanName; 
+}
+
+std::string Bean::getName() const
+{
+    return beanName;
+}
+
+/* ============== Ingredients ================ */
+
+/* assignment-operator overload */
+Ingredient& Ingredient::operator=(Ingredient const& other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+    this->bean = new Bean(*other.bean);
+    this->amount = other.amount;
+    return *this;
+}
+
+/* copy-constructor */
+Ingredient::Ingredient(Ingredient const& other)
+{
+    *this = other;
+}
+
+/* user-defined constructor */
+Ingredient::Ingredient(const Bean& inputBean, int inputAmount) 
+{
+    bean = &inputBean; 
+    amount = inputAmount;
+}
+
+Ingredient::~Ingredient()
+{
+    delete bean;
+}
+
+int Ingredient::getAmount() const
+{
+    return amount;
+}
+
+Bean const& Ingredient::getBean() const
+{
+    return *bean;
+}
+
+/* ============== Event Value ================ */
+
+/* user-defined constructor */
+EventValue::EventValue(int inputEventValue) 
+{
+    eventValue = inputEventValue; 
+}
+
+int EventValue::getValue() const
+{
+    return eventValue;
+}
+
+/* ============== Event ================ */
+
+/* assignment-operator overload */
+Event& Event::operator=(Event const& other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+    this->timestamp = other.timestamp;
+    if (other.eventValue == nullptr)
+    {
+        eventValue = nullptr;
+    }
+    else
+    {
+        this->eventValue = new EventValue(*other.eventValue);
+    }
+    this->type = other.type;
+    return *this;
+}
+
+/* copy-constructor */
+Event::Event(Event const& other)
+{
+    *this = other;
+}
+
+/* user-defined constructor */
+Event::Event(std::string inputType, long inputTimestamp, EventValue* inputEventValue) 
+{
+    type = inputType; 
+    timestamp = inputTimestamp; 
+    eventValue = inputEventValue; 
+}
+
+Event::~Event()
+{
+    delete eventValue;
+}
+
+bool Event::hasValue()const
+{
+    if (eventValue == nullptr)
+    {
+        return false;
+    }
+    return true;
+}
+
+long Event::getTimestamp() const 
+{
+    return timestamp;
+}
+
+EventValue* Event::getValue() const
+{
+    return eventValue;
+}
+
+std::string Event::getType() const
+{
+    return type;
+}
+
+/* ============== Roasts ================ */
+
+/* assignment-operator overload */
+Roast& Roast::operator=(Roast const& other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+    this->roastId = other.roastId; 
+    this->beginTimestamp = other.beginTimestamp;
+    this->eventCount = other.eventCount;
+    this->ingredientsCount = other.ingredientsCount;
+    for (int i=0; i<other.eventCount; i++)
+    {
+        this->eventArray[i] = new Event(other.getEvent(i));
+    }
+    for (int i=0; i<other.ingredientsCount; i++)
+    {
+        this->ingredientArray[i] = new Ingredient(other.getIngredient(i));
+    }
+    return *this;
+}
+
+/* copy-constructor */
+Roast::Roast(Roast const& other)
+{
+    *this = other;
+}
+
+/* user-defined constructor */
+Roast::Roast(long inputId, long inputBeginTimestamp) 
+{
+    roastId = inputId; 
+    beginTimestamp = inputBeginTimestamp;
+}
+
+Roast::~Roast()
+{
+    /* delete all events */
+    for (int i=0; i<eventCount; i++)
+    {
+        delete eventArray[i];
+    }
+
+    /* delete all ingredients */
+    for (int i=0; i<ingredientsCount; i++)
+    {
+        delete ingredientArray[i];
+    }
+}
+
+long Roast::getId() const
+{
+    return roastId;
+}
+
+int Roast::getIngredientsCount() const 
+{
+    return ingredientsCount;
+}
+
+int Roast::getEventCount() const
+{
+    return eventCount;
+}
+
+long Roast::getTimestamp() const
+{
+    return beginTimestamp;
+}
+
+void Roast::addEvent(const Event& event) 
+{
+    // saving the object address in eventArray
+    // taking the address of object reference gives the address of the object
+    // since "applying the address-of operator to the reference is the same 
+    // as taking the address of the original object"
+    eventArray[eventCount] = &event; 
+    eventCount++;
+}
+
+void Roast::addIngredient(const Ingredient& ingredient)
+{
+    ingredientArray[ingredientsCount] = &ingredient;
+    ingredientsCount++;
+}
+
+void Roast::removeEventByTimestamp(long eventTimestamp)
+{
+    for (int i=0; i<eventCount; i++)
+    {
+        if (eventArray[i]->getTimestamp() == eventTimestamp)
+        {
+            /* remove this event and replace it with last event in the array*/
+            delete eventArray[i]; // deletes event object and its eventValue object
+            eventCount--;
+            for (int j=i; j<eventCount; j++)
+            {
+                /* shift each element one by one */
+                eventArray[j] = eventArray[j+1];
+                /* set the duplicated pointer at the end of the array to 0 */
+                eventArray[j+1] = nullptr;
+            }
+        } 
+    }
+}
+
+void Roast::removeIngredientByBeanName(std::string beanName)
+{
+    for (int i=0; i<ingredientsCount; i++)
+    {
+        if (ingredientArray[i]->getBean().getName() == beanName)
+        {
+            /* remove this event and replace it with last event in the array*/
+            delete ingredientArray[i]; // deletes ingredient object and its bean object
+            ingredientsCount--;
+            for (int j=i; j<ingredientsCount; j++)
+            {
+                /* shift each element one by one */
+                ingredientArray[j] = ingredientArray[j+1];
+                /* set the duplicated pointer at the end of the array to 0 */
+                ingredientArray[j+1] = nullptr;
+            }
+        } 
+    }
+}
+
+Event const& Roast::getEvent(int number) const
+{
+    return *eventArray[number];
+}
+
+Ingredient const& Roast::getIngredient(int number) const
+{
+    return *ingredientArray[number];
+}
+
+
--- Source/Model/RoastyModel.hpp
+++ Source/Model/RoastyModel.hpp
@@ -1,1 +1,90 @@
+/* Roasty Model Header */
 #pragma once
+
+#include <string>
+
+#define EVENT_ARRAY_SIZE 200
+#define INGREDIENT_ARRAY_SIZE 200
+#define DEFAULT_EVENT_VALUE 0
+
+/* ============== Bean ================= */
+class Bean 
+{
+public:
+    Bean(std::string inputBeanName); // constructor
+    std::string getName() const; 
+private:
+    std::string beanName; 
+};
+
+/* ============== Ingredients ================ */
+class Ingredient
+{
+public:
+    Ingredient& operator=(Ingredient const& other); // overload assignment operator
+    Ingredient(Ingredient const& other); // copy-constructor
+    Ingredient(const Bean& inputBean, int inputNewAmount); // constructor 
+    ~Ingredient(); // destructor
+    int getAmount() const; 
+    Bean const& getBean() const;
+private:
+    int amount; 
+    const Bean* bean; 
+};
+
+// ============== Event Value ================ */
+class EventValue
+{
+public:
+    EventValue(int inputEventValue); // constructor
+    int getValue() const; 
+private:
+    int eventValue; 
+};
+
+/* ============== Event ================ */
+class Event
+{
+public:
+    Event& operator=(Event const& other); // overload assignment operator
+    Event(Event const& other); // copy-constructor
+    Event(std::string inputType, long inputTimestamp, EventValue* inputEventValue = nullptr); // constructor
+    ~Event(); // destructor
+    bool hasValue()const; 
+    long getTimestamp() const; 
+    EventValue* getValue() const; 
+    std::string getType() const; 
+    
+private:
+    EventValue* eventValue; 
+    long timestamp; 
+    std::string type; 
+
+};
+
+/* ============== Roasts ================ */
+class Roast 
+{
+public:
+    Roast& operator=(Roast const& other); // overload assignment operator
+    Roast(Roast const& other); // copy-constructor
+    Roast(long inputId, long inputBeginTimestamp); // constructor
+    ~Roast(); // destructor
+    long getId() const; 
+    int getIngredientsCount() const; 
+    int getEventCount() const; 
+    long getTimestamp() const; 
+    void addEvent(const Event& event); // i dont think the function needs to be const
+    void addIngredient(const Ingredient& ingredient); 
+    void removeEventByTimestamp(long eventTimestamp); 
+    void removeIngredientByBeanName(std::string beanName); 
+    Event const& getEvent(int number) const; // needs to return reference
+    Ingredient const& getIngredient(int number) const; // needs to return reference
+private:
+    long roastId; 
+    long beginTimestamp; 
+    int eventCount = 0;
+    int ingredientsCount = 0;
+    const Event* eventArray[EVENT_ARRAY_SIZE]; 
+    const Ingredient* ingredientArray[INGREDIENT_ARRAY_SIZE];
+};
