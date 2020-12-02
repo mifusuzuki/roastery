43742951da41801a9b320c05523c79cb1b75f233
Mifu Suzuki <ms1518@texel43.doc.ic.ac.uk>
2020-12-02 13:02:51 +0000

    improve comment / provide white spaces

--- Source/Model/RoastyModel.hpp
+++ Source/Model/RoastyModel.hpp
@@ -17,14 +17,19 @@ class Bean
 public:
     /* Constructor */
     Bean(std::string inputBeanName); 
+
     /* Copy-constructor */
     Bean(Bean const& other); 
+
     /* Overload assignment operator */
     Bean& operator=(Bean const& other); 
+
     /* Destructor */
     ~Bean(){} 
+
     /* Getter function for beanName */
     std::string getName() const; 
+
 private:
     std::string beanName; 
 };
@@ -39,16 +44,23 @@ class Ingredient
 public:
     /* Constructor */   
     Ingredient(Bean& inputBean, int inputNewAmount); 
+
     /* Copy-constructor */
     Ingredient(Ingredient const& other); 
+
     /* Overload assignment operator */
     Ingredient& operator=(Ingredient const& other); 
-    /* Destructor */
+
+    /* Destructor that deletes all owned objects
+    i.e. Bean object */ 
     ~Ingredient();
+
     /* Getter function for amount */
     int getAmount() const; 
+
     /* Getter function for bean object */
     Bean const& getBean() const;
+
 private:
     int amount; 
     Bean& bean; // Owned by Ingredient object
@@ -64,14 +76,19 @@ class EventValue
 public:
     /* Constructor */
     EventValue(int inputEventValue); 
+
     /* Copy-constructor */
     EventValue(EventValue const& other); 
+
     /* Overload assignment operator */
     EventValue& operator=(EventValue const& other);
+
     /* Destructor */
     ~EventValue(){} 
+
     /* Getter function for eventValue */
     int getValue() const; 
+
 private:
     int eventValue; 
 };
@@ -86,20 +103,31 @@ class Event
 public:
     /* Constructor */
     Event(std::string inputType, long inputTimestamp, EventValue* inputEventValue = nullptr); 
+
     /* Copy-constructor */
     Event(Event const& other); 
+
     /* Overload assignment operator */
     Event& operator=(Event const& other);
-    /* Utility function to transfer all class member data from existing event to new/existing event */
+
+    /* Utility function to transfer all class member
+     data from existing event to new/existing event */
     void dataTransfer(Event const& other);
-    /* Destructor */ 
+
+    /* Destructor that deletes all owned objects
+    i.e. EventValue object */  
     ~Event(); 
-    /* Bool check if object has eventValue - returns true if it does and false otherwise */
+
+    /* Bool check if object has eventValue and
+    returns true if it does and false otherwise */
     bool hasValue()const; 
+
     /* Getter function for timestamp */
     long getTimestamp() const; 
+
     /* Getter function for eventValue object */
     EventValue* getValue() const; 
+
     /* Getter function for type */
     std::string getType() const; 
     
@@ -120,28 +148,55 @@ class Roast
 public:
     /* Constructor */
     Roast(long inputId, long inputBeginTimestamp); 
+
     /* Copy-constructor */
     Roast(Roast const& other); 
+
     /* Overload assignment operator */
     Roast& operator=(Roast const& other);
-    /* Utility function to transfer all class member data from existing roast to new/existing roast */
+
+    /* Utility function to transfer all class member
+    data from existing roast to new/existing roast */
     void dataTransfer(Roast const& other);
-    /* Destructor */ 
+
+    /* Destructor that deletes all owned objects
+    i.e. Event and Ingredient objects */ 
     ~Roast(); 
+
     /* Getter function for roastId */
     long getId() const; 
+
     /* Getter function for beginTimeStamp */
     long getTimestamp() const;
+
     /* Getter function for eventCount */
     int getEventCount() const; 
+
     /* Getter function for ingredientCount */
     int getIngredientsCount() const;
+
+    /* Take the owneship of the passed event object
+    & take a pointer to it and add it to eventArray */
     void addEvent(const Event& event); 
-    void addIngredient(const Ingredient& ingredient); 
+
+    /* Take the owneship of the passed ingredient object
+    & take a pointer to it and add it to ingredientArray */
+    void addIngredient(const Ingredient& ingredient);
+
+    /* Destroy the target event object and take its pointer
+    off of eventArray & decrement eventCount by one */
     void removeEventByTimestamp(long eventTimestamp); 
+
+    /* Destroy the target ingredient object and take its pointer
+    off of eventArray & decrement eventCount by one */
     void removeIngredientByBeanName(std::string beanName); 
+
+    /* Getter function for a specified event object in eventArray */
     Event const& getEvent(int number) const; 
+
+    /* Getter function for a specified ingredient object in ingredeintArray */
     Ingredient const& getIngredient(int number) const; 
+
 private:
     long roastId; 
     long beginTimestamp; 
@@ -149,8 +204,10 @@ private:
     int ingredientsCount;
     int eventArrayCapacity = INITIAL_ARRAY_SIZE;
     int ingredientArrayCapacity = INITIAL_ARRAY_SIZE;
+
     /* Dynamically allocated array containing pointers to dynamically allocated Event objects */
     const Event* * eventArray = new const Event*[eventArrayCapacity]; // Owned by Roast object
+    
     /* Dynamically allocated array containing pointers to dynamically allocated Ingredient objects */
     const Ingredient* * ingredientArray = new const Ingredient*[ingredientArrayCapacity]; // Owned by Roast object
     
